e2981ca435173af7ba97d17070515809da0bd277
IanPC <iancaminhas25@hotmail.com>
2022-01-09 19:57:32 -0300

    List of users

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,62 @@
 import React from 'react'
-import {Text} from 'react-native'
+import { View, FlatList, Alert } from 'react-native'
+import { ListItem, Button, Icon } from 'react-native-elements'
+import users from  '../data/users'
 
-export default props =>{
-    return(
-        <Text>UserList</Text>
+export default props => {
+
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                  console.warn('delete '+ user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </>
+        )
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                leftAvatar={{source: {uri: user.avatarUrl}}}
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                bottomDivider
+                rightElement={getActions(user)}
+                onPress={() => props.navigation.navigate('UserForm', user)}
+            />
+        )
+    }
+
+    return (
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
